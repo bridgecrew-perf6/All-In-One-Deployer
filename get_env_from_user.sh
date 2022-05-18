a6b01810fb6dbcd845076720f5181299140682af
addean <najmuddin@plisca.com.my>
2022-05-18 22:30:47 +0800

    add more input to create .env

--- get_env_from_user.sh
+++ get_env_from_user.sh
@@ -20,31 +20,59 @@ if [ -f .env ]
         fi
 fi
 
-read -p "Enter project name : " APP_NAME
+read -p "Enter project name (small letters) : " APP_NAME
 read -p "Enter remote IP or hostname : " HOSTS
 read -p "Enter remote username : " SERVERUSERNAMES
 read -p "Enter remote password : " SERVERPASSWORDS
 read -p "Enter new deployer name : " REMOTEACCOUNT
 read -p "Enter new deployer password : " REMOTEPASSWORD
 echo -e "Select Database type \n 1. PostgreSQL \n 2. MySQL"
-read -p "Enter database type : " DB_TYPE
+read -p "Enter your choice : " DB_TYPE
+if [ "$DB_TYPE" = 1 ]
+    then
+        echo "PostgreSQL Selected"
+elif [ "$DB_TYPE" = 2 ]
+    then
+        echo "MySQL Selected"
+else
+    echo "Choice not exist. Please re-start"
+    exit
+fi
+read -p "Enter database name : " DB_DATABASE
 read -p "Enter database username : " DB_USERNAME
 read -p "Enter database password : " DB_PASSWORD
-read -p "Enter database name : " DB_DATABASE
+read -p "Enter deploy path : " DEPLOY_PATH
+read -p "Enter repository url : " GIT_URL
+read -p "Enter repository branch : " BRANCH
+echo -e "Select Project language \n 1. PHP \n 2. Python"
+read -p "Enter your choice : " LANGUAGE
+if [ "$LANGUAGE" = 1 ]
+    then
+        read -p "Enter PHP version : " VERSION
+elif [ "$LANGUAGE" = 2 ]
+    then
+        read -p "Enter Python version : " VERSION
+else
+    echo "Choice not exist. Please re-start"
+    exit
+fi
+
+echo -e "Select Project Framework \n 1. Laravel \n 2. Django \n 3. FastAPI \n 2. CakePHP"
+read -p "Enter your choice : " FRAMEWORK
 
 # save into .env
 echo "# Project name" >> .env
 echo "APP_NAME=$APP_NAME" >> .env
-# echo "" >> .env
+echo "" >> .env
 echo "# variables to connect server" >> .env
 echo "HOSTS=$HOSTS" >> .env
 echo "SERVERUSERNAMES=$SERVERUSERNAMES" >> .env
 echo "SERVERPASSWORDS=$SERVERPASSWORDS" >> .env
-# echo "" >> .env
+echo "" >> .env
 echo "# variable for create new deployer user" >> .env
 echo "REMOTEACCOUNT=$REMOTEACCOUNT" >> .env
 echo "REMOTEPASSWORD=$REMOTEPASSWORD" >> .env
-# echo "" >> .env
+echo "" >> .env
 echo "# Database" >> .env
 if [ "$DB_TYPE" = 1 ]
     then
@@ -61,7 +89,46 @@ echo "DB_USERNAME=$DB_USERNAME" >> .env
 echo "DB_PASSWORD=$DB_PASSWORD" >> .env
 echo "DB_DATABASE=$DB_DATABASE" >> .env
 
+# GIT
+echo "" >> .env
+echo "DEPLOY_PATH=$DEPLOY_PATH" >> .env
+echo "GIT_URL=$GIT_URL" >> .env
+echo "BRANCH=$BRANCH" >> .env
+
+# language
+echo "" >> .env
+echo "# Language settings" >> .env
+if [ "$LANGUAGE" = 1 ]
+    then
+        echo "PHP=1" >> .env
+elif [ "$LANGUAGE" = 2 ]
+    then
+        echo "PYTHON=1" >> .env
+else
+    echo "Please re-start"
+fi
+
+echo "VERSION=$VERSION" >> .env
+
+# framework
+echo "" >> .env
+echo "# Framework settings" >> .env
+if [ "$FRAMEWORK" = 1 ]
+    then
+        echo "Laravel=1" >> .env
+elif [ "$FRAMEWORK" = 2 ]
+    then
+        echo "Django=1" >> .env
+elif [ "$FRAMEWORK" = 3 ]
+    then
+        echo "FastAPI=1" >> .env
+elif [ "$FRAMEWORK" = 4 ]
+    then
+        echo "CakePHP=1" >> .env
+else
+    echo "Please re-start"
+fi
+
 # sshpass -p !!plisca123SSH scp export_env.sh root@188.166.187.92:/tmp/
-sshpass -p ${SERVERPASSWORDS} scp .export_env.sh ${SERVERUSERNAMES}@${HOSTS}:/tmp/
+sshpass -p ${SERVERPASSWORDS} scp .env ${SERVERUSERNAMES}@${HOSTS}:/tmp/
 # sshpass -p !!plisca123SSH ssh root@188.166.187.92 'cd /tmp; ./export_env.sh'
-sshpass -p ${SERVERPASSWORDS} ssh -l ${SERVERUSERNAMES} ${HOSTS} "ls -al"
